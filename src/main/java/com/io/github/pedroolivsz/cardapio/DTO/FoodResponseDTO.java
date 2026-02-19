3c5eba9a37d10006783c786c4dc28d99ab0204f5
João Pedro <joaopedropereira2209@gmail.com>
2026-02-18 22:08:30 -0300

    feat(foodRepository): created FoodResponseDTO

---
+++ src/main/java/com/io/github/pedroolivsz/cardapio/DTO/FoodResponseDTO.java
@@ -0,0 +1,11 @@
+package com.io.github.pedroolivsz.cardapio.DTO;
+
+import com.io.github.pedroolivsz.cardapio.entity.Food;
+
+import java.math.BigDecimal;
+
+public record FoodResponseDTO(Long id, String title, String image, BigDecimal price) {
+    public FoodResponseDTO(Food food) {
+        this(food.getId(), food.getTitle(), food.getImage(), food.getPrice());
+    }
+}
