b990d5eb16298dfdcface4a8fe9bb4526ceff60c
João Pedro <joaopedropereira2209@gmail.com>
2026-02-18 22:07:10 -0300

    feat(foodRepository): created entity food

--- src/main/java/com/io/github/pedroolivsz/cardapio/entity/Food.java
+++ src/main/java/com/io/github/pedroolivsz/cardapio/entity/Food.java
@@ -1,33 +1,20 @@
 package com.io.github.pedroolivsz.cardapio.entity;
 
 import jakarta.persistence.*;
-import lombok.Getter;
-import lombok.Setter;
+import lombok.*;
 
 import java.math.BigDecimal;
 
 @Entity
 @Table(name = "foods")
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(of = "id")
 public class Food {
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    @Getter
-    @Setter
     private String title;
-    @Getter
-    @Setter
     private String image;
-    @Getter
-    @Setter
     private BigDecimal price;
-
-    public Food() {
-    }
-
-    public Food(Long id, String title, String image, BigDecimal price) {
-        this.id = id;
-        this.title = title;
-        this.image = image;
-        this.price = price;
-    }
 }
