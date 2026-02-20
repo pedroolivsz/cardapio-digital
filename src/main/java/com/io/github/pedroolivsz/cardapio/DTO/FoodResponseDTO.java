8c541380c512454c3ecb7f83e437603845577414
João Pedro <joaopedropereira2209@gmail.com>
2026-02-20 09:50:57 -0300

    refactor(FoodResponseDTO): Updated price attribute

--- src/main/java/com/io/github/pedroolivsz/cardapio/DTO/FoodResponseDTO.java
+++ src/main/java/com/io/github/pedroolivsz/cardapio/DTO/FoodResponseDTO.java
@@ -2,9 +2,7 @@ package com.io.github.pedroolivsz.cardapio.DTO;
 
 import com.io.github.pedroolivsz.cardapio.entity.Food;
 
-import java.math.BigDecimal;
-
-public record FoodResponseDTO(Long id, String title, String image, BigDecimal price) {
+public record FoodResponseDTO(Long id, String title, String image, Integer price) {
     public FoodResponseDTO(Food food) {
         this(food.getId(), food.getTitle(), food.getImage(), food.getPrice());
     }
